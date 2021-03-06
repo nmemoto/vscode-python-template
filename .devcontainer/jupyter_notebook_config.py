85de2697f4b3de4533f1d77ac64de87f052ac4c1
Takafumi Umemoto <takafumi.umemoto@gmail.com>
2021-03-06 15:18:32 +0000

    Initial Commit

---
+++ .devcontainer/jupyter_notebook_config.py
@@ -0,0 +1,4 @@
+c.ServerApp.open_browser = False
+c.ServerApp.ip = '0.0.0.0'
+c.ServerApp.token = 'fae3dd6b17d632927055744fc9523998a4ee0114e2e319268cab9abf957c45ae'
+c.ServerApp.root_dir='/workspace'
